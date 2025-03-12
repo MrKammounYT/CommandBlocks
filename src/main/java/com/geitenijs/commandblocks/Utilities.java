c882beca1b00a503fac5228f8eb4f55bbeb06816
Ahmed Kammoun <67042273+MrKammounYT@users.noreply.github.com>
2025-03-12 16:18:17 +0100

    - Changed the project to gradle - added Libs folder to support Vault - updated the code so it supports version from 1.16.5 to 1.21.4

--- src/main/java/com/geitenijs/commandblocks/Utilities.java
+++ src/main/java/com/geitenijs/commandblocks/Utilities.java
@@ -13,6 +13,7 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -49,86 +50,64 @@ public class Utilities {
     }
 
     static void createConfigs() {
-        List<String> configComments = new ArrayList<>();
-        configComments.addAll(Strings.ASCIILOGO);
-        configComments.addAll(Arrays.asList(
-                "Executing your commands since 2018! By " + Strings.AUTHOR,
-                "Information & Support: " + Strings.WEBSITE,
-                "",
-                "general:",
-                "  pluginbanner: Whether or not to display the fancy banner in your console on server startup.",
-                "  colourfulconsole: Console messages will be coloured when this is enabled.",
-                "updates:",
-                "  check: When enabled, the plugin will check for updates. No automatic downloads, just a subtle notification in the console.",
-                "  notify: Would you like to get an in-game reminder of a new update? Requires permission 'commandblocks.notify.update'.",
-                "default:"
-        ));
-        config.options().setHeader(configComments);
-        configComments.addAll(Strings.BLOCKDEFAULTS);
-        config.set("general.debug", null);
+        FileConfiguration config = YamlConfiguration.loadConfiguration(configFile);
+        FileConfiguration blocks = YamlConfiguration.loadConfiguration(blocksFile);
+
+        // Manually setting default values
         config.addDefault("general.pluginbanner", true);
         config.addDefault("general.colourfulconsole", true);
         config.addDefault("updates.check", true);
         config.addDefault("updates.notify", true);
 
-        List<String> successCommandsConsole = new ArrayList<>();
-        List<String> successCommandsPlayer = new ArrayList<>();
-        List<String> successMessages = new ArrayList<>();
-        List<String> permissionCommandsConsole = new ArrayList<>();
-        List<String> permissionCommandsPlayer = new ArrayList<>();
-        List<String> permissionMessages = new ArrayList<>();
-        List<String> costCommandsConsole = new ArrayList<>();
-        List<String> costCommandsPlayer = new ArrayList<>();
-        List<String> costMessages = new ArrayList<>();
-        List<String> timeoutCommandsConsole = new ArrayList<>();
-        List<String> timeoutCommandsPlayer = new ArrayList<>();
-        List<String> timeoutMessages = new ArrayList<>();
-
-        successCommandsConsole.add("say {player} used a CommandBlock!");
-        successCommandsPlayer.add("me I used a CommandBlock!");
-        permissionMessages.add("&cYou don't have permission to do that.");
-        costMessages.add("&cYou don't have sufficient funds to do that.");
-        timeoutMessages.add("&cPlease wait {time} seconds before doing that again.");
-
-        config.addDefault("default.success.commands.console", successCommandsConsole);
-        config.addDefault("default.success.commands.player", successCommandsPlayer);
-        config.addDefault("default.success.messages", successMessages);
         config.addDefault("default.permission.value", "commandblocks.use");
-        config.addDefault("default.permission.commands.console", permissionCommandsConsole);
-        config.addDefault("default.permission.commands.player", permissionCommandsPlayer);
-        config.addDefault("default.permission.messages", permissionMessages);
         config.addDefault("default.cost.value", 0);
         config.addDefault("default.cost.bypasspermission", "commandblocks.cost.bypass");
-        config.addDefault("default.cost.commands.console", costCommandsConsole);
-        config.addDefault("default.cost.commands.player", costCommandsPlayer);
-        config.addDefault("default.cost.messages", costMessages);
         config.addDefault("default.timeout.value", 5);
         config.addDefault("default.timeout.bypasspermission", "commandblocks.timeout.bypass");
-        config.addDefault("default.timeout.commands.console", timeoutCommandsConsole);
-        config.addDefault("default.timeout.commands.player", timeoutCommandsPlayer);
-        config.addDefault("default.timeout.messages", timeoutMessages);
         config.addDefault("default.delay.value", 0);
 
-        List<String> blocksComments = new ArrayList<>();
-        blocksComments.addAll(Strings.ASCIILOGO);
-        blocksComments.addAll(Arrays.asList(
-                "Executing your commands since 2018! By " + Strings.AUTHOR,
-                "Information & Support: " + Strings.WEBSITE,
-                "",
-                "block:",
-                "  location: The location of the CommandBlock. Don't change this unless you know what you're doing!"
-        ));
-        blocksComments.addAll(Strings.BLOCKDEFAULTS);
-        blocks.options().setHeader(blocksComments);
-        config.options().parseComments(true);
+        // Enable copying defaults
         config.options().copyDefaults(true);
-        blocks.options().parseComments(true);
         blocks.options().copyDefaults(true);
+
+        // Save the configuration file
         saveConfigFile();
         reloadConfigFile();
         saveBlocksFile();
         reloadBlocksFile();
     }
+
+    // Manually adding comments (since setHeader() is not available)
+    static void saveConfigFile() {
+        try {
+            // Write comments manually
+            List<String> comments = Arrays.asList(
+                    "# Executing your commands since 2018! By " + Strings.AUTHOR,
+                    "# Information & Support: " + Strings.WEBSITE,
+                    "#",
+                    "# general:",
+                    "#   pluginbanner: Whether or not to display the fancy banner in your console on server startup.",
+                    "#   colourfulconsole: Console messages will be colored when this is enabled.",
+                    "# updates:",
+                    "#   check: When enabled, the plugin will check for updates.",
+                    "#   notify: Would you like to get an in-game reminder of a new update?",
+                    "# default:"
+            );
+
+            // Write the comments manually before saving
+            FileWriter writer = new FileWriter(configFile);
+            for (String comment : comments) {
+                writer.write(comment + "\n");
+            }
+
+            //save the actual YAML configuration
+            writer.write(config.saveToString());
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     static void registerCommandsAndCompletions() {
         Main.plugin.getCommand("commandblocks").setExecutor(new CommandWrapper());
@@ -220,16 +199,7 @@ public class Utilities {
         config = YamlConfiguration.loadConfiguration(configFile);
     }
 
-    static void saveConfigFile() {
-        if (config == null || configFile == null) {
-            return;
-        }
-        try {
-            config.save(configFile);
-        } catch (IOException ex) {
-            Bukkit.getLogger().log(Level.SEVERE, "Could not save " + configFile, ex);
-        }
-    }
+
 
     public static void reloadBlocksFile() {
         if (blocksFile == null) {
